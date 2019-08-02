dfb4380425ebbba0780135ee141a957d9abc30fe
matteov <matteo.volpi@epitech.eu>
2019-08-02 16:50:02 +0200

    fixes on UI buttons, add button ressources

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+UI = require("src/UI")
+
 SceneType = {
     Menu = 0,
     Options = 1,
@@ -11,13 +13,29 @@ function love.load(args)
     SCENE = SceneType.Menu
     FONT = love.graphics.newFont("ressources/champifont.ttf", 20, "normal")
     love.graphics.setFont(FONT)
+
+    Buttons = {}
+    local buttonPlay = UI.newButton({100, 100}, "ressources/button1.png", {190, 49})
+    table.insert(Buttons, buttonPlay)
+    local buttonOptions = UI.newButton({100, 300}, "ressources/button2.png", {190, 49})
+    table.insert(Buttons, buttonOptions)
+    local buttonCredit = UI.newButton({100, 500}, "ressources/button1.png", {190, 49})
+    table.insert(Buttons, buttonCredit)
 end
 
 function love.update(dt)
+    if SCENE == SceneType.Menu then
+        for i = 1, #Buttons do
+            Buttons[i].checkEvent()
+        end
+    end
 end
 
 function love.draw()
     if SCENE == SceneType.Menu then
+        for i = 1, #Buttons do
+            Buttons[i].draw()
+        end
     elseif SCENE == SceneType.Credit then
         love.graphics.print("Created by Ryugha and pl0xxxy", WIN_SIZE.width / 2, WIN_SIZE.height / 2 - 20)
     end
--- src/UI.lua
+++ src/UI.lua
@@ -1,15 +1,16 @@
 local UI = {}
 
-function UI.newButton(coord, spritesheet, rect)
+function UI.newButton(coord, spritesheet, rect, scale)
     local Button = {}
     Button.sprite = love.graphics.newImage(spritesheet)
     Button.coord = coord
     Button.rect = rect
+    Button.scale = scale or {1, 1}
     Button.quads = {}
     Button.toDraw = 1
 
-    for y = 0, Button.sprite:getHeight() - Button.rect[1], Button.rect[1] do
-        for x = 0, Button.sprite:getWidth() - Button.rect[2], Button.rect[2] do
+    for y = 0, Button.sprite:getWidth() - Button.rect[1], Button.rect[1] do
+        for x = 0, Button.sprite:getHeight() - Button.rect[2], Button.rect[2] do
             table.insert(Button.quads, love.graphics.newQuad(x, y,
                 Button.rect[1], Button.rect[2], Button.sprite:getDimensions()))
         end
@@ -20,20 +21,16 @@ function UI.newButton(coord, spritesheet, rect)
     end
 
     Button.draw = function()
-        local scaleX = 1.5
-        local scaleY = 1.5
-        if Button.toDraw == 2 then
-            scaleX = 1.7
-            scaleY = 1.7
-        end
-        love.graphics.draw(Button.sprite, Button.quads[Button.toDraw], Button.coord[1], Button.coord[2],
-    0, scaleX, scaleY, 0, 0, 0, 0)
+        love.graphics.draw(Button.sprite, Button.quads[Button.toDraw], Button.coord[1],
+        Button.coord[2], 0, Button.scale[1], Button.scale[2], 0, 0, 0, 0)
     end
 
     Button.checkEvent = function()
         local x, y = love.mouse.getPosition()
-        if x >= Button.coord[1] and x <= Button.rect[1] * 1.5 and
-        y >= Button.coord[2] and y <= Button.rect[2] * 1.5 then
+
+        if x >= Button.coord[1] and x <= Button.coord[1] + Button.rect[1] and
+        y >= Button.coord[2] and y <= Button.coord[2] + Button.rect[2] and
+        love.mouse.isDown(1) then
             Button.toDraw = 2
         else
             Button.toDraw = 1
